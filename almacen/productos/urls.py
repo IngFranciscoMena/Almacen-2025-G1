030c3a7b0326b3ad4c131e6740487e9433d90550
IngFranciscoMena <franciscodaniel.penate@unab.edu.sv>
2025-09-30 20:34:16 -0600

    third commit: categorias

--- almacen/productos/urls.py
+++ almacen/productos/urls.py
@@ -2,7 +2,12 @@
 from django.urls import path
 # importar las vistas
 from .views import (
-    ProductoListView
+    # Producto
+    ProductoListView,
+    # Categoria
+    CategoriaListView,
+    CategoriaCreateView,
+    
 )
 
 # nombre descriptivo para las url
@@ -11,4 +16,6 @@ app_name = "productos"
 # crear el enrutamiento de las url
 urlpatterns = [
     path('', ProductoListView.as_view(), name="producto-list"),
+    path('categoria/', CategoriaListView.as_view(), name="categoria-list"),
+    path('categoria/nuevo', CategoriaCreateView.as_view(), name="categoria-create")
 ]
--- almacen/productos/views.py
+++ almacen/productos/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 # importar vistas genericas
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView
 # importar las clases
-from .models import Producto
+from .models import Producto, Categoria
+# importar método reverse_lazy
+from django.urls import reverse_lazy
 
 # Create your views here.
 
@@ -15,4 +17,18 @@ class ProductoListView(ListView):
     template_name = "producto/producto-list.html"
     # nombre del contexto del objetos
     context_object_name = "productos"
+    
+    
+# crear una clase generica para mostrar el listado de categorias
+class CategoriaListView(ListView):
+    
+    model = Categoria
+    template_name = "categoria/categoria-list.html"
+    context_object_name = "categorias"
 
+class CategoriaCreateView(CreateView):
+    
+    model = Categoria
+    fields = ["nombre", "descripcion"]
+    template_name = "categoria/categoria-form.html"
+    success_url = reverse_lazy("productos:categoria-list")
